5d47bd92a5550d306cfabf2c7ebdf98129cb23c4
Ollisteka <zhukova.o.m@yandex.ru>
2017-09-27 22:41:14 +0500

    Added tests for snake

--- src/tests/SnakeTests.java
+++ src/tests/SnakeTests.java
@@ -7,6 +7,9 @@ public class SnakeTests {
     @Test
     public void testCreate(){
         Snake snake = new Snake(1,2);
-        Assert.assertEquals(snake.getHead(), new Location(1,2));
+        Assert.assertEquals(new Location(1,2), snake.getHead());
+        Assert.assertEquals(1, snake.Length());
+        Assert.assertEquals(Direction.Stop, snake.direction);
+        Assert.assertEquals(1, snake.Body.size());
     }
 }
