731f41fb42cc3e40272b47be661154894280258d
Ollisteka <zhukova.o.m@yandex.ru>
2017-09-28 20:45:21 +0500

    Tested snake movement

--- src/tests/SnakeTests.java
+++ src/tests/SnakeTests.java
@@ -1,15 +1,31 @@
-package tests;
-import main.*;
+import main.Direction;
+import main.GameState;
+import main.Location;
+import main.Snake;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SnakeTests {
-    @Test
-    public void testCreate(){
-        Snake snake = new Snake(1,2);
-        Assert.assertEquals(new Location(1,2), snake.getHead());
-        Assert.assertEquals(1, snake.Length());
-        Assert.assertEquals(Direction.Stop, snake.direction);
-        Assert.assertEquals(1, snake.Body.size());
-    }
+
+  @Test
+  public void testCreate() {
+    Snake snake = new Snake(1, 2);
+    Assert.assertEquals(new Location(1, 2), snake.getHead());
+    Assert.assertEquals(1, snake.Length());
+    Assert.assertEquals(Direction.Stop, snake.direction);
+    Assert.assertEquals(1, snake.Body.size());
+  }
+
+  @Test
+  public void testMove() {
+    GameState game = new GameState(10, 10, new Snake(0, 0));
+    Assert.assertEquals(false, game.IsOver);
+    Assert.assertEquals(0, game.Scores);
+    Assert.assertEquals(new Location(0, 0), game.Snake.getHead());
+
+    game.Snake.Move(Direction.Right, game);
+    Assert.assertEquals(new Location(1, 0), game.Snake.getHead());
+    Assert.assertEquals(null, game.Map.getObject(0, 0).Snake);
+    Assert.assertEquals(1, game.Snake.Length());
+  }
 }
