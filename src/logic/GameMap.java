89f14c51860a813c96f60cc90d8a3e130df96f92
Gladkix <y.gladkix@mail.ru>
2017-10-03 23:11:08 +0500

    some refactor

--- src/logic/GameMap.java
+++ src/logic/GameMap.java
@@ -10,17 +10,13 @@ public class GameMap {
   private MapObject[][] map;
   private Random rnd = new Random();
 
-  public GameMap(int width, int height) {
-    int[][] a = new int[width][height];
-    map = new MapObject[width][height];
-    makeLevelOne();
-  }
-  public GameMap(int width, int height, boolean cycled) {
+  public GameMap(int width, int height, boolean cycled=false) {
     int[][] a = new int[width][height];
     map = new MapObject[width][height];
     isCycled = cycled;
     makeLevelOne();
   }
+  // trying to work with git :)
 
   public int Width() {
     return map[0].length;
@@ -30,7 +26,7 @@ public class GameMap {
     return map[1].length;
   }
 
-  public void setObject(int x, int y, MapObject object) {
+  public void setObject(int x, int y, MapObject object) { // maybe better to use Location than "int x, int y"
     if (x >= Width() || y >= Height() || x < 0 || y < 0) {
       throw new IndexOutOfBoundsException();
     }
@@ -55,11 +51,10 @@ public class GameMap {
       if (map[x][y].Snake == null && map[x][y].Wall == null && map[x][y].Food == null) {
         Location result = new Location(x, y);
         map[x][y].Food = new Food(result, 10, poison);
-        if (!poison) {
+        if (!poison)
           foodCount++;
-        } else {
+        else
           poisonCount++;
-        }
         return result;
       }
     }
