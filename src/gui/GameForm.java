4e9ad5159daa47fe90f9e2a76c90b5e5d0fdc15b
Ollisteka <zhukova.o.m@yandex.ru>
2017-10-05 21:36:05 +0500

    added test menu bar

--- src/gui/GameForm.java
+++ src/gui/GameForm.java
@@ -1,12 +1,17 @@
 package gui;
 
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import logic.GameState;
 
 public class GameForm implements Runnable {
 
   private JFrame frame;
   private GameFieldPanel gameFieldPanel;
+  private JMenuBar menuBar = new JMenuBar();
 
   private GameState game;
 
@@ -16,12 +21,25 @@ public class GameForm implements Runnable {
 
   }
 
+  private void initializeMenu(){
+    menuBar = new JMenuBar();
+    JMenu menu = new JMenu("Menu");
+    menuBar.add(menu);
+    JMenuItem item = new JMenuItem("Test Item");
+    menu.add(item);
+  }
+
   @Override
   public void run() {
     frame = new JFrame("Snake");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     gameFieldPanel = new GameFieldPanel(game, frame);
+    initializeMenu();
+
     frame.add(gameFieldPanel);
+    frame.setJMenuBar(menuBar);
+
     frame.pack();
     frame.setLocationByPlatform(true);
     frame.setVisible(true);
